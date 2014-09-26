c8a61920e190d5332f50b29265bd4973ce366cbc
Hayden Waisanen <haydenzone@gmail.com>
2014-09-25 21:05:53 -0700

    Added ability to delete posts.

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ console.log(tumblr_api_read);
 var app = angular.module('TumblrApp', []);
 app.controller('PostsController', function() {
     this.posts = tumblr_api_read.posts;
+    this.deletePost = function(index) {
+        this.posts[index].delete_ = true;
+        this.posts[index].liked = false;
+    };
 });
